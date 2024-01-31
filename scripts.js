4c1ce91ecac13d6282483e287b2cc0f890d62db8
leadgfl13 <leadgfl13@gmail.com>
2024-01-30 22:37:02 -0500

    Create gameboard iffe that makes a grid with squares

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 document.addEventListener('click',()=>{
     alert("Hello")
 })
+
+const gameboard = (function(){
+    let board = document.createElement('div')
+    board.setAttribute('id','board')
+    document.body.append(board)
+    for(let i=0; i<=9; i++){
+        let square = document.createElement('div')
+        square.setAttribute('id','square')
+        board.append(square)
+    }
+
+})();
