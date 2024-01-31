4a640dd16955c9b196357d0208099338583ea3c1
leadgfl13 <leadgfl13@gmail.com>
2024-01-31 18:19:30 -0500

    Add event listener that switches player for every click on a square

--- scripts.js
+++ scripts.js
@@ -1,15 +1,36 @@
-document.addEventListener('click',()=>{
-    alert("Hello")
-})
+function createPlayer(playername, mark){
+    const marker = mark
+    const name = playername
+    return{name, marker}
+}
+
+let player1 = createPlayer('Gary','X')
+let player2 = createPlayer('Mo', 'O')
 
 const gameboard = (function(){
+    let currentPlayer = player1
     let board = document.createElement('div')
     board.setAttribute('id','board')
     document.body.append(board)
-    for(let i=0; i<=9; i++){
+    for(let i=0; i<9; i++){
         let square = document.createElement('div')
         square.setAttribute('id','square')
         board.append(square)
-    }
+        square.innerHTML = i
+        square.addEventListener('click',()=>{
+            square.innerHTML = currentPlayer.marker
+            if(currentPlayer == player1){
+                currentPlayer = player2
+            }
+         else if (currentPlayer == player2){
+            currentPlayer = player1
+        } })
+        }
 
 })();
+
+
+//function to switch currentPlayer
+
+
+
