0cc7c0327b2f6ef649483c0d8efb0c4560e2dfaa
leadgfl13 <leadgfl13@gmail.com>
2024-01-31 21:59:21 -0500

    Add function to check for win conditions

--- scripts.js
+++ scripts.js
@@ -11,12 +11,13 @@ const gameboard = (function(){
     let currentPlayer = player1
     let board = document.createElement('div')
     board.setAttribute('id','board')
+     squares = []
     document.body.append(board)
     for(let i=0; i<9; i++){
         let square = document.createElement('div')
         square.setAttribute('id','square')
+        squares.push(square)
         board.append(square)
-        square.innerHTML = i
         square.addEventListener('click',()=>{
             square.innerHTML = currentPlayer.marker
             if(currentPlayer == player1){
@@ -24,10 +25,22 @@ const gameboard = (function(){
             }
          else if (currentPlayer == player2){
             currentPlayer = player1
-        } })
+            
+        }    check() }
+        
+        )
+    }
         }
+)
 
-})();
+();
+
+function check(){
+    if(squares[0].innerHTML == 'X' && squares[1].innerHTML == 'X' && squares[2].innerHTML == 'X'){
+        console.log("Hello")
+    }
+}
+
 
 
 //function to switch currentPlayer
