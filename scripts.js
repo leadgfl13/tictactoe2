69193967bb07a0fde07db3ddecf69363da874f88
leadgfl13 <leadgfl13@gmail.com>
2024-02-01 22:00:05 -0500

    Add functions to get player names

--- scripts.js
+++ scripts.js
@@ -7,7 +7,20 @@ function createPlayer(playername, mark){
 let player1 = createPlayer('Gary','X')
 let player2 = createPlayer('Mo', 'O')
 
+function getPlayer1(){
+    player1.name = prompt('Name of player 1')
+    return player1.name
+}
+
+function getPlayer2(){
+    player2.name = prompt("Name of player 2")
+}
+
 const gameboard = (function(){
+
+getPlayer1()
+getPlayer2()
+    
     let currentPlayer = player1
     let board = document.createElement('div')
     board.setAttribute('id','board')
@@ -47,7 +60,7 @@ function check(){
     squares[1].innerHTML == 'X' && squares[4].innerHTML == 'X' && squares[7].innerHTML == 'X' ||
     squares[2].innerHTML == 'X' && squares[5].innerHTML == 'X' && squares[8].innerHTML == 'X' 
     )
-     console.log('player 1 wins!')
+     alert(`${player1.name} wins!`)
 
     if (squares[0].innerHTML == 'O' && squares[1].innerHTML == 'O' && squares[2].innerHTML == 'O' ||
      squares[3].innerHTML == 'O' && squares[4].innerHTML == 'O' && squares[5].innerHTML == 'O' ||
@@ -57,12 +70,12 @@ function check(){
      squares[0].innerHTML == 'O' && squares[3].innerHTML == 'O' && squares[6].innerHTML == 'O'||
      squares[1].innerHTML == 'O' && squares[4].innerHTML == 'O' && squares[7].innerHTML == 'O' ||
      squares[2].innerHTML == 'O' && squares[5].innerHTML == 'O' && squares[8].innerHTML == 'O')
-     console.log('player2 wins')
+     alert(`${player2.name} wins!`)
 
     
     else if (squares[0].innerHTML !='' && squares[1].innerHTML!='' && squares[2].innerHTML!='' && squares[3].innerHTML!=''
      && squares[4].innerHTML!='' && squares[5].innerHTML!='' &&squares[6].innerHTML!='' && squares[7].innerHTML!='' && squares[8].innerHTML!=''){
-            console.log('tie')
+            Alert.log('tie')
         }
     }
 
